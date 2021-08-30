6771b8feaa3625d93cfc169e5ef0f4ba9001c5b4
malom <malom@DESKTOP-JP5R3HB>
2021-08-30 15:33:50 -0500

    Actualizacion de Controlador, Modelo y Vista Pais

---
+++ SisPicWEB/Controllers/PaisController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SisPicBD;
+
+namespace SisPicWEB.Controllers
+{
+    public class PaisController : Controller
+    {
+        private SISPICEntities db = new SISPICEntities();
+
+        // GET: Pais
+        public ActionResult Index()
+        {
+            return View(db.Pais.ToList());
+        }
+
+        // GET: Pais/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pais pais = db.Pais.Find(id);
+            if (pais == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pais);
+        }
+
+        // GET: Pais/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pais/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "pai_id,pai_codigo,pai_nombre,pai_codigotelefonico,pai_iso2,pai_iso3")] Pais pais)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pais.Add(pais);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pais);
+        }
+
+        // GET: Pais/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pais pais = db.Pais.Find(id);
+            if (pais == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pais);
+        }
+
+        // POST: Pais/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "pai_id,pai_codigo,pai_nombre,pai_codigotelefonico,pai_iso2,pai_iso3")] Pais pais)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pais).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pais);
+        }
+
+        // GET: Pais/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pais pais = db.Pais.Find(id);
+            if (pais == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pais);
+        }
+
+        // POST: Pais/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pais pais = db.Pais.Find(id);
+            db.Pais.Remove(pais);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
