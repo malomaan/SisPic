3b07f3e13c204335c1e17d98c53ed8331be16de5
malom <malom@DESKTOP-JP5R3HB>
2021-08-30 15:20:18 -0500

    Actualizar modelo y proyecto WEB SisPicWEB

---
+++ SisPicWEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SisPicWEB.Startup))]
+namespace SisPicWEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
